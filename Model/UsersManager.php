0a080a1ac4d2fda491250ac1338ffefca833116b
SickMyDucks <me@neilrichter.com>
2018-02-26 14:38:07 +0000

    Users can now log in

--- Model/UsersManager.php
+++ Model/UsersManager.php
@@ -1,6 +1,6 @@
 
 <?php
-require_once('Cool/DBManager.php');
+require_once 'Cool/DBManager.php';
 
 class UsersManager
 {
@@ -11,18 +11,18 @@ class UsersManager
         if (strlen($username) < 4) {
             $errors['username'] = "Username too short";
             $isFormValid = false;
-        } 
+        }
         /*if (!empty($usernameExisting)) {
-            $username_error = 'Username already taken, choose another one.';
-            $isFormValid = false;
-        }*/ 
+        $username_error = 'Username already taken, choose another one.';
+        $isFormValid = false;
+        }*/
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $errors['email'] = "Invalid email !";
             $isFormValid = false;
-        } 
+        }
         /*elseif (!empty($existingEmail)) {
-            $email_error = "Email already in use on our site";
-            $isFormValid = false;
+        $email_error = "Email already in use on our site";
+        $isFormValid = false;
         } */
         if (strlen($password) < 8) {
             $errors['password'] = 'Password too short';
@@ -44,14 +44,28 @@ class UsersManager
         $stmt->bindParam(':username', $username);
         $stmt->bindParam(':email', $email);
         $stmt->bindParam(':hashedPassword', $hashedPassword);
-        
+
         $stmt->execute();
 
         return $errors;
     }
 
-    public function login()
+    public function login($username, $password)
     {
+        $dbm = DBManager::getInstance();
+        $pdo = $dbm->getPdo();
 
+        $result = $pdo->query("SELECT * FROM users WHERE username = '$username'");
+        $user = $result->fetch();
+        $hash = $user['hashedPassword'];
+        if (password_verify($password, $hash)) {
+            session_start();
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+            header('Location: ?action=files');
+            exit();
+        } else {
+            return 'Username or password is invalid.';
+        }
     }
 }
