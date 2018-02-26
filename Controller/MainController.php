982383ca7808abd83aa72b612840ee06162c6c7c
SickMyDucks <me@neilrichter.com>
2018-02-26 12:12:55 +0000

    Created registerAction()

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -8,4 +8,25 @@ class MainController extends BaseController
     {
         return $this->render('home.html.twig');
     }
+
+    public function registerAction()
+    {
+        $errors = ['none'];
+        require_once('Model/UsersManager.php');
+        if(isset($_POST['username']) && isset($_POST['first-name'])
+        && isset($_POST['last-name']) && isset($_POST['email'])
+        && isset($_POST['password']) && isset($_POST['password-repeat'])
+        && isset($_POST['submit'])){
+            $username = htmlentities($_POST['username']);
+            $firstName = htmlentities($_POST['first-name']);
+            $lastName = htmlentities($_POST['last-name']);
+            $email = htmlentities($_POST['email']);
+            $password = htmlentities($_POST['password']);
+            $passwordRepeat = htmlentities($_POST['password-repeat']);
+            $manager = new UsersManager();
+            $errors = $manager-> register($username, $firstName, $lastName, $email, $password, $passwordRepeat);
+            $logs = ['errors' => $errors];
+        }
+        return $this->render('register.html.twig', $logs);
+    }
 }
